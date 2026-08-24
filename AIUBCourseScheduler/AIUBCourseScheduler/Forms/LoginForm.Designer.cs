d4dc5885d672179fa50d95683570afb8412d6b92
abir-aldin <atulpabna@gmail.com>
2026-08-25 02:22:10 +0600

    Create initial application structure and user controls
    Added login, admin and student main forms with separate admin and student user control structures.

--- AIUBCourseScheduler/AIUBCourseScheduler/Forms/LoginForm.Designer.cs
+++ AIUBCourseScheduler/AIUBCourseScheduler/Forms/LoginForm.Designer.cs
@@ -1,14 +1,14 @@
-﻿namespace AdminUser1
+﻿namespace AIUBCourseScheduler.Forms
 {
-    partial class Form1
+    partial class LoginForm
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,8 +23,8 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
@@ -34,21 +34,21 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(250, 125);
+            label1.Font = new Font("Papyrus", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(178, 162);
             label1.Name = "label1";
-            label1.Size = new Size(251, 46);
+            label1.Size = new Size(431, 94);
             label1.TabIndex = 0;
             label1.Text = "Welcome Dear";
             // 
-            // Form1
+            // LoginForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(847, 465);
             Controls.Add(label1);
-            Name = "Form1";
-            Text = "Form1";
+            Name = "LoginForm";
+            Text = "LoginForm";
             ResumeLayout(false);
             PerformLayout();
         }
--- AIUBCourseScheduler/AIUBCourseScheduler/Program.cs
+++ AIUBCourseScheduler/AIUBCourseScheduler/Program.cs
@@ -1,4 +1,6 @@
-namespace AdminUser1
+using AIUBCourseScheduler.Forms;
+
+namespace AIUBCourseScheduler
 {
     internal static class Program
     {
@@ -11,7 +13,7 @@ namespace AdminUser1
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
