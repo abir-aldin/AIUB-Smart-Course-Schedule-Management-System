f54a143f105864f2025989afe5b35ac04231d211
abir-aldin <atulpabna@gmail.com>
2026-08-25 12:14:05 +0600

    Update LoginForm.Designer.cs

--- AIUBCourseScheduler/AIUBCourseScheduler/Forms/LoginForm.Designer.cs
+++ AIUBCourseScheduler/AIUBCourseScheduler/Forms/LoginForm.Designer.cs
@@ -35,11 +35,11 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Papyrus", 48F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(224, 155);
+            label1.Location = new Point(116, 153);
             label1.Name = "label1";
-            label1.Size = new Size(372, 126);
+            label1.Size = new Size(621, 126);
             label1.TabIndex = 0;
-            label1.Text = "Welcome";
+            label1.Text = "Welcome SAS";
             // 
             // LoginForm
             // 
