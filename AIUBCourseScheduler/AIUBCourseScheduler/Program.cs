6fc3fab7843240fa62046bfa5db6607b6e441316
Sikrity <sikrityahmed07@gmail.com>
2026-08-26 14:01:33 +0600

    admin profile

--- AIUBCourseScheduler/AIUBCourseScheduler/Program.cs
+++ AIUBCourseScheduler/AIUBCourseScheduler/Program.cs
@@ -1,4 +1,5 @@
 using AIUBCourseScheduler.Forms;
+using AIUBCourseScheduler.UserControls.Admin;
 
 namespace AIUBCourseScheduler
 {
@@ -13,7 +14,8 @@ namespace AIUBCourseScheduler
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new LoginForm());
+            //Application.Run(new LoginForm());
+            Application.Run(new AdminProfileControl());
         }
     }
 }
--- AIUBCourseScheduler/AIUBCourseScheduler/UserControls/Admin/AdminProfileControl.Designer.cs
+++ AIUBCourseScheduler/AIUBCourseScheduler/UserControls/Admin/AdminProfileControl.Designer.cs
@@ -28,12 +28,457 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AdminProfileControl";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label11 = new Label();
+            pictureBox1 = new PictureBox();
+            label10 = new Label();
+            label12 = new Label();
+            button2 = new Button();
+            pictureBox2 = new PictureBox();
+            button1 = new Button();
+            pictureBox3 = new PictureBox();
+            textBox1 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            button3 = new Button();
+            pictureBox4 = new PictureBox();
+            label13 = new Label();
+            label14 = new Label();
+            pictureBox5 = new PictureBox();
+            label15 = new Label();
+            label16 = new Label();
+            button4 = new Button();
+            pictureBox6 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Navy;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(200, 38);
+            label1.TabIndex = 1;
+            label1.Text = "Admin Profile";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.GrayText;
+            label2.Location = new Point(25, 47);
+            label2.Name = "label2";
+            label2.Size = new Size(440, 20);
+            label2.TabIndex = 2;
+            label2.Text = "View and manage your account information and security settings.";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(25, 92);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 20);
+            label3.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.InactiveBorder;
+            textBox2.Location = new Point(364, 197);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(276, 36);
+            textBox2.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(264, 133);
+            label6.Name = "label6";
+            label6.Size = new Size(87, 100);
+            label6.TabIndex = 7;
+            label6.Text = "\r\n\r\n\r\n\r\nFull Name :";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(264, 140);
+            label5.Name = "label5";
+            label5.Size = new Size(81, 40);
+            label5.TabIndex = 6;
+            label5.Text = "\r\nAdmin ID :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Enabled = false;
+            label4.Font = new Font("Microsoft Sans Serif", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Navy;
+            label4.Location = new Point(12, 79);
+            label4.Name = "label4";
+            label4.Size = new Size(170, 44);
+            label4.TabIndex = 5;
+            label4.Text = " \r\nAccount Summary";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            label4.Click += label4_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(264, 267);
+            label7.Name = "label7";
+            label7.Size = new Size(54, 20);
+            label7.TabIndex = 11;
+            label7.Text = "Email :";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(264, 321);
+            label8.Name = "label8";
+            label8.Size = new Size(47, 20);
+            label8.TabIndex = 12;
+            label8.Text = "Role :";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(264, 372);
+            label9.Name = "label9";
+            label9.Size = new Size(99, 20);
+            label9.TabIndex = 13;
+            label9.Text = "Department :";
+            label9.Click += label9_Click;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.Location = new Point(264, 439);
+            label11.Name = "label11";
+            label11.Size = new Size(61, 20);
+            label11.TabIndex = 15;
+            label11.Text = "Phone :";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.pngtree_avatar_icon_profile_icon_member_login_vector_isolated_png_image_5247852;
+            pictureBox1.Location = new Point(25, 191);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(171, 170);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 22;
+            pictureBox1.TabStop = false;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(70, 376);
+            label10.Name = "label10";
+            label10.Size = new Size(64, 23);
+            label10.TabIndex = 23;
+            label10.Text = "Admin";
+            label10.Click += label10_Click_1;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.ForeColor = SystemColors.GrayText;
+            label12.Location = new Point(31, 399);
+            label12.Name = "label12";
+            label12.Size = new Size(151, 20);
+            label12.TabIndex = 24;
+            label12.Text = "System Administrator";
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.RoyalBlue;
+            button2.Location = new Point(31, 568);
+            button2.Name = "button2";
+            button2.Size = new Size(192, 62);
+            button2.TabIndex = 25;
+            button2.Text = "Edit Profile";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.images1;
+            pictureBox2.Location = new Point(46, 581);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(36, 30);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 26;
+            pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.RoyalBlue;
+            button1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(323, 568);
+            button1.Name = "button1";
+            button1.Size = new Size(301, 62);
+            button1.TabIndex = 27;
+            button1.Text = "Save Changes";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.istockphoto_2190953346_612x612;
+            pictureBox3.Location = new Point(364, 581);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(43, 36);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 28;
+            pictureBox3.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.InactiveBorder;
+            textBox1.Location = new Point(364, 144);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(276, 36);
+            textBox1.TabIndex = 29;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.InactiveBorder;
+            textBox3.Location = new Point(364, 251);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(276, 36);
+            textBox3.TabIndex = 30;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.InactiveBorder;
+            textBox4.Location = new Point(364, 305);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(276, 36);
+            textBox4.TabIndex = 31;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = SystemColors.InactiveBorder;
+            textBox5.Location = new Point(364, 356);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(276, 36);
+            textBox5.TabIndex = 32;
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = SystemColors.InactiveBorder;
+            textBox6.Location = new Point(364, 423);
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(276, 36);
+            textBox6.TabIndex = 33;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(685, 568);
+            button3.Name = "button3";
+            button3.Size = new Size(149, 62);
+            button3.TabIndex = 34;
+            button3.Text = "Cancel";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.images__1_;
+            pictureBox4.Location = new Point(702, 584);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(29, 33);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 35;
+            pictureBox4.TabStop = false;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label13.ForeColor = Color.Navy;
+            label13.Location = new Point(724, 121);
+            label13.Name = "label13";
+            label13.Size = new Size(75, 23);
+            label13.TabIndex = 36;
+            label13.Text = "Security";
+            label13.Click += label13_Click;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label14.Location = new Point(754, 278);
+            label14.Name = "label14";
+            label14.Size = new Size(146, 23);
+            label14.TabIndex = 37;
+            label14.Text = "Change Password";
+            label14.Click += label14_Click;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.istockphoto_2204974549_612x612;
+            pictureBox5.Location = new Point(771, 186);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(94, 89);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 38;
+            pictureBox5.TabStop = false;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.ForeColor = SystemColors.GrayText;
+            label15.Location = new Point(711, 301);
+            label15.Name = "label15";
+            label15.Size = new Size(224, 20);
+            label15.TabIndex = 39;
+            label15.Text = "Update your password regularly ";
+            label15.Click += label15_Click;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.ForeColor = SystemColors.GrayText;
+            label16.Location = new Point(724, 321);
+            label16.Name = "label16";
+            label16.Size = new Size(197, 20);
+            label16.TabIndex = 40;
+            label16.Text = "to keep your account secure.";
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.RoyalBlue;
+            button4.Location = new Point(724, 356);
+            button4.Name = "button4";
+            button4.Size = new Size(176, 43);
+            button4.TabIndex = 41;
+            button4.Text = "Change Password";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = Properties.Resources.images__2_;
+            pictureBox6.Location = new Point(724, 362);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(22, 30);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 42;
+            pictureBox6.TabStop = false;
+            // 
+            // AdminProfileControl
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(963, 737);
+            Controls.Add(pictureBox6);
+            Controls.Add(button4);
+            Controls.Add(label16);
+            Controls.Add(label15);
+            Controls.Add(pictureBox5);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(pictureBox4);
+            Controls.Add(button3);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox3);
+            Controls.Add(button1);
+            Controls.Add(pictureBox2);
+            Controls.Add(button2);
+            Controls.Add(label12);
+            Controls.Add(label10);
+            Controls.Add(pictureBox1);
+            Controls.Add(label11);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label6);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "AdminProfileControl";
+            Text = "AdminProfileControl";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label11;
+        private PictureBox pictureBox1;
+        private Label label10;
+        private Label label12;
+        private Button button2;
+        private PictureBox pictureBox2;
+        private Button button1;
+        private PictureBox pictureBox3;
+        private TextBox textBox1;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Button button3;
+        private PictureBox pictureBox4;
+        private Label label13;
+        private Label label14;
+        private PictureBox pictureBox5;
+        private Label label15;
+        private Label label16;
+        private Button button4;
+        private PictureBox pictureBox6;
     }
 }
