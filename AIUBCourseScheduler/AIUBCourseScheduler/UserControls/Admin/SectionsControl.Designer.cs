34d3aff047d9c2177a44a73ee75697cbe63cce73
sunmimaman420-bit <sunmimaman420@gmail.com>
2026-08-27 23:00:45 +0600

    SectionControlDesignCreated

--- AIUBCourseScheduler/AIUBCourseScheduler/UserControls/Admin/SectionsControl.Designer.cs
+++ AIUBCourseScheduler/AIUBCourseScheduler/UserControls/Admin/SectionsControl.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SectionsControl";
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            textBox1 = new TextBox();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI Black", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Blue;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(350, 50);
+            label1.TabIndex = 0;
+            label1.Text = "Section Management";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ControlDarkDark;
+            label2.Location = new Point(12, 49);
+            label2.Name = "label2";
+            label2.Size = new Size(301, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Create,view, and manage all course section";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.HotTrack;
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(823, 22);
+            button1.Name = "button1";
+            button1.Size = new Size(128, 47);
+            button1.TabIndex = 2;
+            button1.Text = "+  Add Section\r\n";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(textBox1);
+            flowLayoutPanel1.Location = new Point(11, 81);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(940, 79);
+            flowLayoutPanel1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(3, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // SectionsControl
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(963, 737);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "SectionsControl";
+            Text = "SectionsControl";
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private TextBox textBox1;
     }
 }
