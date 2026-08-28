2bd687daeeeec495828152efc6e83b4e750c2c75
abir-aldin <atulpabna@gmail.com>
2026-08-28 14:48:08 +0600

    Add database connection and setup script

---
+++ AIUBCourseScheduler/AIUBCourseScheduler/DataAccess/DatabaseConnection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace AIUBCourseScheduler.DataAccess
+{
+    public static class DatabaseConnection
+    {
+        private const string ConnectionString =
+            @"Server=.\SQLEXPRESS;
+              Database=AIUBCourseScheduleDB;
+              Integrated Security=True;
+              TrustServerCertificate=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
