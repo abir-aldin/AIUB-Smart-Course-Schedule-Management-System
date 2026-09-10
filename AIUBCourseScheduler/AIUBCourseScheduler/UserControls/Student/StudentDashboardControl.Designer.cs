a9ec51b7d3df6b22065eeaa36748ba9d57c767e0
sunmimaman420-bit <sunmimaman420@gmail.com>
2026-09-10 23:24:01 +0600

    tyio

--- AIUBCourseScheduler/AIUBCourseScheduler/UserControls/Student/StudentDashboardControl.Designer.cs
+++ AIUBCourseScheduler/AIUBCourseScheduler/UserControls/Student/StudentDashboardControl.Designer.cs
@@ -28,12 +28,413 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "StudentDashboardControl";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StudentDashboardControl));
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            panel4 = new Panel();
+            label11 = new Label();
+            label7 = new Label();
+            panel5 = new Panel();
+            label12 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            panel1 = new Panel();
+            label9 = new Label();
+            label4 = new Label();
+            panel2 = new Panel();
+            label10 = new Label();
+            label5 = new Label();
+            panel3 = new Panel();
+            label13 = new Label();
+            label6 = new Label();
+            panel6 = new Panel();
+            label14 = new Label();
+            label8 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            dataGridView1 = new DataGridView();
+            scheduleColumn = new DataGridViewTextBoxColumn();
+            CreatedDateColumn = new DataGridViewTextBoxColumn();
+            StatusColumn = new DataGridViewButtonColumn();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Thistle;
+            panel4.Controls.Add(label11);
+            panel4.Controls.Add(label7);
+            panel4.Location = new Point(1256, 108);
+            panel4.Margin = new Padding(5);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(380, 194);
+            panel4.TabIndex = 5;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Microsoft JhengHei UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.Location = new Point(280, 104);
+            label11.Margin = new Padding(5, 0, 5, 0);
+            label11.Name = "label11";
+            label11.Size = new Size(32, 36);
+            label11.TabIndex = 6;
+            label11.Text = "2";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Microsoft JhengHei UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(178, 19);
+            label7.Margin = new Padding(5, 0, 5, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(103, 46);
+            label7.TabIndex = 2;
+            label7.Text = "Approved \r\nSchedules\r\n";
+            // 
+            // panel5
+            // 
+            panel5.BackColor = SystemColors.Control;
+            panel5.Controls.Add(label12);
+            panel5.Location = new Point(28, 336);
+            panel5.Margin = new Padding(5);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(0, 0);
+            panel5.TabIndex = 6;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.Location = new Point(23, 16);
+            label12.Margin = new Padding(5, 0, 5, 0);
+            label12.Name = "label12";
+            label12.Size = new Size(281, 31);
+            label12.TabIndex = 0;
+            label12.Text = "Recent Schedule Requests";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = SystemColors.HotTrack;
+            label1.Location = new Point(12, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(319, 31);
+            label1.TabIndex = 7;
+            label1.Text = "Welcome to Your Dashboard";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ControlDarkDark;
+            label2.Location = new Point(12, 45);
+            label2.Name = "label2";
+            label2.Size = new Size(428, 23);
+            label2.TabIndex = 8;
+            label2.Text = "Manage your courses and keep track of your schedules\r\n";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ButtonHighlight;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(label4);
+            panel1.Location = new Point(12, 71);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(216, 125);
+            panel1.TabIndex = 9;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Microsoft JhengHei", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.FromArgb(128, 128, 255);
+            label9.Location = new Point(156, 66);
+            label9.Name = "label9";
+            label9.Size = new Size(32, 36);
+            label9.TabIndex = 1;
+            label9.Text = "5";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(132, 10);
+            label4.Name = "label4";
+            label4.Size = new Size(81, 50);
+            label4.TabIndex = 0;
+            label4.Text = "selected\r\nCourses\r\n";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ButtonHighlight;
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(label10);
+            panel2.Controls.Add(label5);
+            panel2.Location = new Point(247, 71);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(217, 125);
+            panel2.TabIndex = 10;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Microsoft JhengHei", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.ForeColor = Color.LimeGreen;
+            label10.Location = new Point(146, 66);
+            label10.Name = "label10";
+            label10.Size = new Size(32, 36);
+            label10.TabIndex = 2;
+            label10.Text = "3";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(112, 10);
+            label5.Name = "label5";
+            label5.Size = new Size(105, 50);
+            label5.TabIndex = 1;
+            label5.Text = "Generated \r\nSchedules\r\n";
+            label5.Click += label5_Click;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.ButtonHighlight;
+            panel3.Controls.Add(pictureBox3);
+            panel3.Controls.Add(label13);
+            panel3.Controls.Add(label6);
+            panel3.Location = new Point(482, 71);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(222, 125);
+            panel3.TabIndex = 11;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Microsoft JhengHei", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label13.ForeColor = Color.Coral;
+            label13.Location = new Point(153, 66);
+            label13.Name = "label13";
+            label13.Size = new Size(32, 36);
+            label13.TabIndex = 2;
+            label13.Text = "1";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(131, 10);
+            label6.Name = "label6";
+            label6.Size = new Size(88, 75);
+            label6.TabIndex = 1;
+            label6.Text = "Pending \r\nRequests\r\n\r\n";
+            // 
+            // panel6
+            // 
+            panel6.BackColor = SystemColors.ButtonHighlight;
+            panel6.Controls.Add(pictureBox4);
+            panel6.Controls.Add(label14);
+            panel6.Controls.Add(label8);
+            panel6.Location = new Point(725, 71);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(215, 125);
+            panel6.TabIndex = 12;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Microsoft JhengHei", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label14.ForeColor = Color.BlueViolet;
+            label14.Location = new Point(143, 66);
+            label14.Name = "label14";
+            label14.Size = new Size(32, 36);
+            label14.TabIndex = 2;
+            label14.Text = "2";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(113, 10);
+            label8.Name = "label8";
+            label8.Size = new Size(99, 75);
+            label8.TabIndex = 1;
+            label8.Text = "Approved \r\nSchedules\r\n\r\n";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(16, 10);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(108, 92);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(12, 10);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(101, 92);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(19, 10);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(106, 99);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(3, 10);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(105, 99);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(5, 471);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(951, 254);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 14;
+            pictureBox5.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { scheduleColumn, CreatedDateColumn, StatusColumn });
+            dataGridView1.Location = new Point(15, 212);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(925, 253);
+            dataGridView1.TabIndex = 15;
+            // 
+            // scheduleColumn
+            // 
+            scheduleColumn.HeaderText = "Schedule";
+            scheduleColumn.MinimumWidth = 6;
+            scheduleColumn.Name = "scheduleColumn";
+            // 
+            // CreatedDateColumn
+            // 
+            CreatedDateColumn.HeaderText = "Created Date";
+            CreatedDateColumn.MinimumWidth = 6;
+            CreatedDateColumn.Name = "CreatedDateColumn";
+            // 
+            // StatusColumn
+            // 
+            StatusColumn.HeaderText = "Status";
+            StatusColumn.MinimumWidth = 6;
+            StatusColumn.Name = "StatusColumn";
+            // 
+            // StudentDashboardControl
+            // 
+            AutoScaleDimensions = new SizeF(14F, 31F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLight;
+            ClientSize = new Size(963, 737);
+            Controls.Add(dataGridView1);
+            Controls.Add(pictureBox5);
+            Controls.Add(panel6);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Margin = new Padding(5);
+            Name = "StudentDashboardControl";
+            Text = "StudentDashboardControl";
+            Load += StudentDashboardControl_Load;
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Panel panel4;
+        private Label label11;
+        private Label label7;
+        private Panel panel5;
+        private Label label12;
+        private Label label1;
+        private Label label2;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel6;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label13;
+        private Label label14;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn scheduleColumn;
+        private DataGridViewTextBoxColumn CreatedDateColumn;
+        private DataGridViewButtonColumn StatusColumn;
     }
 }
--- AIUBCourseScheduler/AIUBCourseScheduler/UserControls/Student/StudentDashboardControl.cs
+++ AIUBCourseScheduler/AIUBCourseScheduler/UserControls/Student/StudentDashboardControl.cs
@@ -14,5 +14,25 @@ namespace AIUBCourseScheduler.UserControls.Student
         {
             InitializeComponent();
         }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void StudentDashboardControl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
